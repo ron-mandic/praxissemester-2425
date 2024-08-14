30774d31ed24dcaf1cb61c81742a22a775c35a9c
ron-mandic <ron.mandic@hfg.design>
2024-08-14 18:15:29 +0200

    0x004 - First project of Three.js Journey

---
+++ 08-2024/kw-33/threejs-journey-03-first-lesson/main.js
@@ -0,0 +1,26 @@
+import "./style.css";
+import * as THREE from "three";
+
+const canvas = document.querySelector("canvas.webgl");
+console.log(canvas);
+
+// Scene
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, 800 / 600);
+camera.position.z = 3;
+
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+	canvas,
+});
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
