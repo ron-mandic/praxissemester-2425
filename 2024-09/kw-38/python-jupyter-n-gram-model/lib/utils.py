b6b598f8fb92548abc210f23cdaed7a0caa899cf
ron-mandic <ron.mandic@hfg.design>
2024-09-19 19:11:24 +0200

    0x024 - Implementing my own SLM

---
+++ 2024-09/kw-38/python-jupyter-n-gram-model/lib/utils.py
@@ -0,0 +1,73 @@
+import json, re
+
+def load(path: str) -> dict:
+    with open(path, 'r') as f:
+        return json.load(f)
+    
+def preprocess(str: str) -> str:
+    # Remove the first part with the title 
+    reg0 = re.compile(r'^.* Lyrics\n')
+    str0 = reg0.sub('', str)
+    # Remove the end part
+    reg1 = re.compile(r'\d+Embed$')
+    str1 = reg1.sub('', str0)
+    # Sanitize str0 by removing all ",", "'", ".", "-", ...
+    regEnd0 = re.compile(r'[\,\'\’\.\!\?\:\;\(\)\[\]\{\}]+')
+    strEnd0 = regEnd0.sub('', str1)
+    regEnd1 = re.compile(r'[\-\—]+')
+    strEnd1 = regEnd1.sub(' ', strEnd0)
+    return strEnd1
+
+def split_into_ngrams(list_words, n) -> list:
+    return [list_words[i:i+n] for i in range(len(list_words)-n+1)]
+
+def get_dict (list_list_words: list, size = 1, reverse = True) -> list:
+    dict = {}
+
+    for list_words in list_list_words:
+        ngrams = split_into_ngrams(list_words, size)
+
+        for ngram in ngrams:
+            key = "+".join(ngram)
+            if key in dict:
+                dict[key] += 1
+            else:
+                dict[key] = 1
+
+    arr_tuple = sorted(dict.items(), key=lambda x: x[1], reverse=reverse)
+    total = sum(value for (_, value) in arr_tuple)
+    # Absolute and relative values (normalized)
+    arr_tuple = [(key, value, value/total) for (key, value) in arr_tuple]
+    # sum(rel for (_, _, rel) in dict_tuple) == 1.0
+
+    return {key: (abs, rel) for (key, abs, rel) in arr_tuple}
+
+def ask_dict(dict_key_tuple: dict, str: str, separator = "+") -> dict:
+    dict = {key: tuple for key, tuple in dict_key_tuple.items() if key.startswith(str+separator)} 
+
+    total = sum([tuple[0] for tuple in dict.values()])
+    return {key: (tuple[0], tuple[1], tuple[0]/total) for key, tuple in dict.items()}
+
+"""
+Source: https://stackoverflow.com/a/2970542
+"""
+def findall(sub, str):
+    count = start = 0
+    while True:
+        start = str.find(sub, start) + 1
+        if start > 0:
+            count+=1
+        else:
+            return count
+        
+def countall(str, list_str):
+    i = 0
+    for str_comp in list_str:
+        i += findall(str, str_comp)
+    return i
+
+def predict(start_tokens: list, ngram_context_window: int):
+    if (len(start_tokens) != ngram_context_window - 1):
+        raise ValueError(f"The start tokens should have a length of {ngram_context_window - 1}")
+    
+    # Get the n-grams that start with the start tokens
